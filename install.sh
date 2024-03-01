1de45a93e0fe0c65b05785863e8b554b735aaaef
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-03-01 20:00:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 echo "nix-config by SchweGELBin"
-echo "Please select your device (The first volume, not any subvolumes):"
-echo "$(lsblk)"
-
-read dev
 
 cd ~
 curl -fs https://raw.githubusercontent.com/SchweGELBin/nix-config/master/disko.nix >> ~/disko.nix
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disko.nix --arg device '"/dev/$dev"'
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disko.nix --arg device '"/dev/$1"'
 
 sudo mkdir -p /mnt/etc
 curl -fs https://raw.githubusercontent.com/SchweGELBin/nix-config/master/shell.nix >> ~/shell.nix
