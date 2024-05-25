302bfd998335ec219644f2516c4487326d7f6c91
SchweGELBin <abramjannikmichael06@gmail.com>
2024-05-25 19:27:41 +0200

    Update Wallpaper

--- configuration.nix
+++ configuration.nix
@@ -225,8 +225,8 @@ stylix = {
     }; 
   };
   image = pkgs.fetchurl {
-    url = "https://w.wallhaven.cc/full/3l/wallhaven-3l828y.jpg";
-    hash = "sha256-hLlEBpwcjKQ6l846hU4OJFIa6uEeH2Zf5ouiZg8rn8A=";
+    url = "https://w.wallhaven.cc/full/2y/wallhaven-2yx5og.jpg";
+    hash = "sha256-BfxglbS7JoJyvtvwFETLWo9mcDjylLmcMpk0vW1AdKI=";
   };
   opacity = {
     popups = 0.7;
--- modules/dotfiles/wallpaper.nix
+++ modules/dotfiles/wallpaper.nix
@@ -1,7 +1,7 @@
 {config, lib, pkgs, ...}:
 {
   home.file."Pictures/Wallpapers/wallpaper.jpg".source = pkgs.fetchurl {
-    url = "https://w.wallhaven.cc/full/3l/wallhaven-3l828y.jpg";
-    hash = "sha256-hLlEBpwcjKQ6l846hU4OJFIa6uEeH2Zf5ouiZg8rn8A=";
+    url = "https://w.wallhaven.cc/full/2y/wallhaven-2yx5og.jpg";
+    hash = "sha256-BfxglbS7JoJyvtvwFETLWo9mcDjylLmcMpk0vW1AdKI=";
   };
 }
