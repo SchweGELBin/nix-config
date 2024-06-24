b7d3e6dd47854a81a9c772e7b7e1f17db2212d18
SchweGELBin <abramjannikmichael06@gmail.com>
2024-06-24 17:31:38 +0200

    configuration: add cachix (nix-community)

--- configuration.nix
+++ configuration.nix
@@ -136,8 +136,14 @@ in
         "flakes"
         "nix-command"
       ];
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
