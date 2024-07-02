de66f2acd3710d944a8e6def9e01fd6d0f41db1b
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-07-02 17:53:15 +0200

    config: delete shell.nix

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
-  pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
-  };
-in
-
-pkgs.mkShellNoCC { packages = with pkgs; [ git ]; }
