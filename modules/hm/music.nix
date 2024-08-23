bb9e5c7526b9ecd9317bc0123deddd55e65f987d
SchweGELBin <abramjannikmichael06@gmail.com>
2024-08-23 14:00:01 +0200

    flake: update

--- modules/hm/music.nix
+++ modules/hm/music.nix
@@ -1,5 +1,6 @@
 {
   home.file.".config/nix/music.list".text = ''
+    https://piped.video/playlist?list=PL0PCz_ViBzWbmK6qG9CnTdvv5LntT32PM
     https://piped.video/playlist?list=PLHi2T2b45lpg_X7uHlNkk8kzycLvc1AAd 
     https://piped.video/playlist?list=PL37UZ2QfPUvz3k5mZNFZmjhLNTT-M5-Oa 
   '';
