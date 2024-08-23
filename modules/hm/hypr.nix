6b0337924460d31ee5111919ef43bcdfb109c66d
SchweGELBin <abramjannikmichael06@gmail.com>
2024-08-23 16:06:06 +0200

    config(hypr): dwindle -> master and workman default

--- modules/hm/hypr.nix
+++ modules/hm/hypr.nix
@@ -50,16 +50,17 @@ in
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     plugins = [
-      (inputs.hyprland-plugins.packages.${pkgs.system}.hyprwinwrap.overrideAttrs { class = "kitty-bg"; })
+      #(inputs.hyprland-plugins.packages.${pkgs.system}.hyprwinwrap.overrideAttrs { class = "kitty-bg"; })
     ];
     settings = {
 
+      "$altMod" = "ALT";
+      "$mainMod" = "SUPER";
       "$applauncher" = "fuzzel";
       "$bar" = "waybar";
       "$browser" = "firefox";
       "$discord" = "vesktop";
       "$filemanager" = "nemo";
-      "$mainMod" = "SUPER";
       "$terminal" = "kitty";
 
       animations = {
@@ -76,78 +77,70 @@ in
       };
 
       bindd = [
-        # Binds
-        ", SUPER_L, Show Keybinds, exec, kitty \"${vars.user.home}/.config/hypr/binds.sh\""
-
-        # General
-        "$mainMod, C, Kill active window, killactive, "
-        "$mainMod, D, Launch Discord client ($discord), exec, $discord"
-        "$mainMod, E, Launch File Manager ($filemanager), exec, $filemanager"
-        "$mainMod, F, Toggle Fullscreen, fullscreen"
-        "$mainMod, J, Toggle h/w window split, togglesplit, "
-        "$mainMod, K, Launch Color Picker, exec, hyprpicker -f hex -a"
-        "$mainMod, L, Launch Rocket League (Sugar), exec, mangohud legendary launch Sugar"
-        "$mainMod, M, Play Music, exec, music"
-        "$mainMod, O, Launch Browser ($browser), exec, $browser"
-        "$mainMod, P, Toggle Pseudo (Keep aspect ratio), pseudo, "
-        "$mainMod, Q, Launch Terminal ($terminal), exec, $terminal"
-        "$mainMod, R, Launch App Launcher ($applauncher), exec, $applauncher"
-        "$mainMod, V, Toggle floating window, togglefloating, "
-        "$mainMod, W, Restart Bar ($bar), exec, pkill $bar && $bar"
-
-        # Focus
-        "$mainMod, down, Move focus down, movefocus, d"
-        "$mainMod, left, Move focus left, movefocus, l"
-        "$mainMod, right, Move focus right, movefocus, r"
-        "$mainMod, up, Move focus up, movefocus, u"
-        "$mainMod, Tab, Move focus to next monitor, focusmonitor, +1"
-        "$mainMod SHIFT, Tab, Move focus to previous monitor, focusmonitor, -1"
-
-        # Workspaces
-        "$mainMod, 1, Go to workspace 1, workspace, 1"
-        "$mainMod, 2, Go to workspace 2, workspace, 2"
-        "$mainMod, 3, Go to workspace 3, workspace, 3"
-        "$mainMod, 4, Go to workspace 4, workspace, 4"
-        "$mainMod, 5, Go to workspace 5, workspace, 5"
-        "$mainMod, 6, Go to workspace 6, workspace, 6"
-        "$mainMod, 7, Go to workspace 7, workspace, 7"
-        "$mainMod, 8, Go to workspace 8, workspace, 8"
-        "$mainMod, 9, Go to workspace 9, workspace, 9"
-        "$mainMod, 0, Go to workspace 10, workspace, 10"
-
-        "$mainMod SHIFT, 1, Move focused window and go to workspace 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, Move focused window and go to workspace 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, Move focused window and go to workspace 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, Move focused window and go to workspace 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, Move focused window and go to workspace 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, Move focused window and go to workspace 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, Move focused window and go to workspace 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, Move focused window and go to workspace 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, Move focused window and go to workspace 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, Move focused window and go to workspace 10, movetoworkspace, 10"
-
-        "$mainMod, S, Toggle special workspace, togglespecialworkspace, magic"
-        "$mainMod SHIFT, S, Move current workspace to special workspace, movetoworkspace, special:magic"
-
-        "$mainMod, mouse_down, Go to next workspace, workspace, e+1"
-        "$mainMod, mouse_up, Go to previous workspace, workspace, e-1"
-
-        # Screenshots
-        ", Print, Take Screenshot of current Display, exec, screenshot d"
-        "SHIFT, Print, Take Screenshot of selected Region, exec, screenshot r"
-        "$mainMod, Print, Take Screenshot of focused Window, exec, screenshot w"
+        "$altMod   SHIFT, up,   Add window to master,                           layoutmsg, addmaster"
+        "$mainMod, mouse_down,  Go to next workspace,                           workspace, e+1"
+        "$mainMod, mouse_up,    Go to previous workspace,                       workspace, e-1"
+        "$mainMod, 0,           Go to workspace 0,                              workspace, 0"
+        "$mainMod, 1,           Go to workspace 1,                              workspace, 1"
+        "$mainMod, 2,           Go to workspace 2,                              workspace, 2"
+        "$mainMod, 3,           Go to workspace 3,                              workspace, 3"
+        "$mainMod, 4,           Go to workspace 4,                              workspace, 4"
+        "$mainMod, 5,           Go to workspace 5,                              workspace, 5"
+        "$mainMod, 6,           Go to workspace 6,                              workspace, 6"
+        "$mainMod, 7,           Go to workspace 7,                              workspace, 7"
+        "$mainMod, 8,           Go to workspace 8,                              workspace, 8"
+        "$mainMod, 9,           Go to workspace 9,                              workspace, 9"
+        "$mainMod, C,           Kill active window,                             killactive, "
+        "$mainMod, Enter,       Launch App Launcher ($applauncher),             exec, $applauncher"
+        "$mainMod, A,           Launch Browser ($browser),                      exec, $browser"
+        "$mainMod, K,           Launch Color Picker,                            exec, hyprpicker -f hex -a"
+        "$mainMod, O,           Launch Discord client ($discord),               exec, $discord"
+        "$mainMod, E,           Launch File Manager ($filemanager),             exec, $filemanager"
+        "$mainMod, K,           Launch Rocket League,                           exec, mangohud legendary launch Sugar"
+        "$mainMod, Q,           Launch Terminal ($terminal),                    exec, $terminal"
+        "$mainMod  SHIFT, S,    Move current workspace to special workspace,    movetoworkspace, special:magic"
+        "$mainMod, down,        Move focus down,                                movefocus, d"
+        "$mainMod, left,        Move focus left,                                movefocus, l"
+        "$mainMod, right,       Move focus right,                               movefocus, r"
+        "$mainMod, up,          Move focus up,                                  movefocus, u"
+        "$altMod,  ALT_L,       Move focus to master or first child,            layoutmsg, focusmaster"
+        "$mainMod, Tab,         Move focus to next monitor,                     focusmonitor, +1"
+        "$altMod,  right,       Move focus to next window,                      layoutmsg, cyclenext"
+        "$mainMod  SHIFT, Tab,  Move focus to previous monitor,                 focusmonitor, -1"
+        "$altMod,  left,        Move focus to previous window,                  layoutmsg, cycleprev"
+        "$mainMod  SHIFT, 0,    Move focused window and go to workspace 0,      movetoworkspace, 0"
+        "$mainMod  SHIFT, 1,    Move focused window and go to workspace 1,      movetoworkspace, 1"
+        "$mainMod  SHIFT, 2,    Move focused window and go to workspace 2,      movetoworkspace, 2"
+        "$mainMod  SHIFT, 3,    Move focused window and go to workspace 3,      movetoworkspace, 3"
+        "$mainMod  SHIFT, 4,    Move focused window and go to workspace 4,      movetoworkspace, 4"
+        "$mainMod  SHIFT, 5,    Move focused window and go to workspace 5,      movetoworkspace, 5"
+        "$mainMod  SHIFT, 6,    Move focused window and go to workspace 6,      movetoworkspace, 6"
+        "$mainMod  SHIFT, 7,    Move focused window and go to workspace 7,      movetoworkspace, 7"
+        "$mainMod  SHIFT, 8,    Move focused window and go to workspace 8,      movetoworkspace, 8"
+        "$mainMod  SHIFT, 9,    Move focused window and go to workspace 9,      movetoworkspace, 9"
+        "$mainMod, L,           Play Music,                                     exec, music"
+        "$altMod   SHIFT, down, Remove window from master,                      layoutmsg, removemaster"
+        "$mainMod, D,           Restart Bar ($bar),                             exec, pkill $bar && $bar"
+        "SUPER,    SUPER_L,     Show Keybinds,                                  exec, kitty \"${vars.user.home}/.config/hypr/binds.sh\""
+        "$altMod,  Tab,         Swap focused window with master or first child, layoutmsg, swapwithmaster"
+        "$altMod,  up,          Swap focused window with next one,              layoutmsg,   swapnext"
+        "$altMod,  down,        Swap focused window with previous one,          layoutmsg, swapprev"
+        ",         Print,       Take Screenshot of current Display,             exec, screenshot d"
+        "$mainMod, Print,       Take Screenshot of focused Window,              exec, screenshot w"
+        "SHIFT,    Print,       Take Screenshot of selected Region,             exec, screenshot r"
+        "$mainMod, H,           Toggle floating window,                         togglefloating, "
+        "$mainMod, T,           Toggle fullscreen,                              fullscreen, "
+        "$mainMod, I,           Toggle h/w window split,                        togglesplit, "
+        "$mainMod, P,           Toggle pseudo (Keep aspect ratio),              pseudo, "
+        "$mainMod, S,           Toggle special workspace,                       togglespecialworkspace, magic"
       ];
 
       binddel = [
-        # Volume
-        ", XF86AudioLowerVolume, Increase Volume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86AudioRaiseVolume, Decrease Volume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, Decrease Volume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioRaiseVolume, Increase Volume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
       ];
 
-      binddl = [
-        # Audio Mute
-        ", XF86AudioMute, Mute Audio, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ];
+      binddl = [ ", XF86AudioMute, Mute Audio, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle" ];
 
       binddm = [
         # Window Resize / Move
@@ -179,7 +172,7 @@ in
 
       exec-once = [
         "hyprpaper"
-        #"kitty -c \"~/.config/kitty/kittybg.conf\" --class=\"kitty-bg\" \"${vars.user.home}/.config/kitty/cavabg.sh\""
+        "kitty -c \"~/.config/kitty/kittybg.conf\" --class=\"kitty-bg\" \"${vars.user.home}/.config/kitty/cavabg.sh\""
         "openrgb -c $accentAlpha"
         "waybar"
       ];
@@ -191,7 +184,7 @@ in
         border_size = 2;
         gaps_in = 5;
         gaps_out = 20;
-        layout = "dwindle";
+        layout = "master";
       };
 
       gestures = {
@@ -203,7 +196,7 @@ in
         force_no_accel = 1;
         kb_layout = "us,us";
         kb_options = "caps:backspace, grp:win_space_toggle";
-        kb_variant = ",workman";
+        kb_variant = "workman,";
         sensitivity = 0;
         touchpad = {
           natural_scroll = "no";
@@ -211,6 +204,8 @@ in
       };
 
       master = {
+        allow_small_split = true;
+        new_on_top = true;
         new_status = "master";
       };
 
