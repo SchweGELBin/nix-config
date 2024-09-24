75c23487cc6d0094737a5da2107a6966b85e90b2
SchweGELBin <abramjannikmichael06@gmail.com>
2024-09-24 19:14:02 +0200

    flake: update
    and a bit with devshells ;)

--- modules/hm/devshells/dioxus.nix
+++ modules/hm/devshells/dioxus.nix
@@ -4,12 +4,13 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     dioxus-cli
+    gradle
     openssl_3
     pkg-config
   ];
   shellHook = ''
     ndk_version=$(grep "^Pkg.Revision" $NDK_HOME/source.properties | awk -F'=' '{print $2}' | tr -d ' ')
     export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_HOME/ndk/$ndk_version/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang
-    cargo install cargo-mobile2
+    #cargo install cargo-mobile2
   '';
 }
