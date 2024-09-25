866c825f5510bfafd163c6e313bea9fc623a4dd9
SchweGELBin <abramjannikmichael06@gmail.com>
2024-09-25 22:00:07 +0200

    devshells(slint): fix openssl

--- modules/hm/devshells/slint.nix
+++ modules/hm/devshells/slint.nix
@@ -1,15 +1,24 @@
 let
   pkgs = import <nixpkgs> { };
-in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
+
+  libraries = with pkgs; [
+    openssl_3
+  ];
+
+  packages = with pkgs; [
     jdk17
     openssl_3
-    pkg-config
   ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+
   shellHook = ''
     ndk_version=$(grep "^Pkg.Revision" $NDK_HOME/source.properties | awk -F'=' '{print $2}' | tr -d ' ')
     export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_HOME/ndk/$ndk_version/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
     export JAVA_HOME=${pkgs.jdk17}/lib/openjdk
     #cargo install cargo-apk
   '';
