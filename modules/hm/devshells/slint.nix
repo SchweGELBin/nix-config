4138e77a0b6ebe859da292e9f257eda643189850
SchweGELBin <abramjannikmichael06@gmail.com>
2024-09-27 16:41:47 +0200

    devshells(slint): remove unnecessary parts, add keystore vars

--- modules/hm/devshells/slint.nix
+++ modules/hm/devshells/slint.nix
@@ -1,17 +1,8 @@
 let
   pkgs = import <nixpkgs> { };
-
-  libraries =
-    with pkgs;
-    [
-    ];
-
-  packages = with pkgs; [
-    jdk17
-  ];
 in
 pkgs.mkShell {
-  buildInputs = packages;
+  buildInputs = with pkgs; [ jdk17 ];
 
   nativeBuildInputs = with pkgs; [ pkg-config ];
 
@@ -20,6 +11,8 @@ pkgs.mkShell {
     export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_HOME/ndk/$ndk_version/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
     export JAVA_HOME=${pkgs.jdk17}/lib/openjdk
+    export CARGO_APK_RELEASE_KEYSTORE=/home/michi/.android/openbible.keystore
+    #export CARGO_APK_RELEASE_KEYSTORE_PASSWORD=
     #cargo install cargo-apk
   '';
 }
