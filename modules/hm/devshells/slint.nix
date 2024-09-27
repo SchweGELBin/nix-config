656ae0665be697b109eaa562feaad0a10cbbf160
SchweGELBin <abramjannikmichael06@gmail.com>
2024-09-27 16:47:20 +0200

    devshells(slint): remove libraries

--- modules/hm/devshells/slint.nix
+++ modules/hm/devshells/slint.nix
@@ -9,7 +9,6 @@ pkgs.mkShell {
   shellHook = ''
     ndk_version=$(grep "^Pkg.Revision" $NDK_HOME/source.properties | awk -F'=' '{print $2}' | tr -d ' ')
     export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_HOME/ndk/$ndk_version/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
     export JAVA_HOME=${pkgs.jdk17}/lib/openjdk
     export CARGO_APK_RELEASE_KEYSTORE=/home/michi/.android/openbible.keystore
     #export CARGO_APK_RELEASE_KEYSTORE_PASSWORD=
