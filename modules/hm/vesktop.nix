a23a7f3f4571ccdf3438c142178c3376dc5587dc
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-16 20:51:36 +0200

    vesktop: cleanup

--- modules/hm/vesktop.nix
+++ modules/hm/vesktop.nix
@@ -1,18 +1,9 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
   cfg = config.vesktop;
-  vars = import ../nix/vars.nix;
 in
 {
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [ vesktop ];
-    home.file.".config/vesktop/settings/quickCss.css".text =
-      "@import url(https://catppuccin.github.io/discord/dist/catppuccin-${vars.cat.flavor}-${vars.cat.accent}.theme.css);";
     programs.vesktop = {
       enable = true;
       settings = {
