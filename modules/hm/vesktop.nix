87691febe14133036b76095cb74a59cae25f9f3a
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-16 17:15:10 +0200

    vesktop: add settings

--- modules/hm/vesktop.nix
+++ modules/hm/vesktop.nix
@@ -13,6 +13,41 @@ in
     home.packages = with pkgs; [ vesktop ];
     home.file.".config/vesktop/settings/quickCss.css".text =
       "@import url(https://catppuccin.github.io/discord/dist/catppuccin-${vars.cat.flavor}-${vars.cat.accent}.theme.css);";
+    programs.vesktop = {
+      enable = true;
+      settings = {
+        arRPC = true;
+        staticTitle = true;
+        clickTrayToShowHide = true;
+        spellCheckLanguages = [
+          "de"
+          "en"
+        ];
+      };
+      vencord.settings = {
+        autoUpdate = false;
+        autoUpdateNotification = false;
+        transparent = false;
+        plugins = {
+          CustomRPC = {
+            enabled = true;
+            appID = "1372946876575387759";
+            appName = "Foxi's Adventure";
+            buttonOneText = "Download Game";
+            buttonOneURL = "https://play.google.com/store/apps/details?id=com.schwegelbin.foxisadventure";
+            buttonTwoText = "Homepage";
+            buttonTwoURL = "https://milchi.site";
+            imageBig = "https://play-lh.googleusercontent.com/SdxJZuzbOXOMv6VnkfyO2Yb_xBw4UII11uTuJmjqJnombs_yUwxG706tRdBFNt73ftym=w480-h960";
+          };
+          CrashHandler = {
+            attemptToPreventCrashes = true;
+            attemptToNavigateToHome = false;
+          };
+          NoTrack.disableAnalytics = true;
+          Settings.settingsLocation = "aboveNitro";
+        };
+      };
+    };
   };
 
   options = {
