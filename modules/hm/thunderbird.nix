8272513bae1b8c7bf583c6911daa215b6b3b24f4
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-19 18:09:39 +0200

    thunderbird: harden

--- modules/hm/thunderbird.nix
+++ modules/hm/thunderbird.nix
@@ -7,7 +7,24 @@ in
   config = lib.mkIf cfg.enable {
     programs.thunderbird = {
       enable = true;
-      profiles.${vars.user.name}.isDefault = true;
+      profiles.${vars.user.name} = {
+        isDefault = true;
+        settings = {
+          "beacon.enabled" = false;
+          "extensions.activeThemeID" = "default-theme@mozilla.org";
+          "gfx.downloadable_fonts.enabled" = false;
+          "layout.css.visited_links_enabled" = false;
+          "mail.inline_attachments" = false;
+          "mailnews.display.html_as" = 1;
+          "mailnews.display.prefer_plaintext" = true;
+          "media.hardware-video-decoding.enabled" = false;
+          "network.cookie.cookieBehavior" = 2;
+          "network.http.referer.XOriginPolicy" = 2;
+          "network.http.sendRefererHeader" = 0;
+          "network.IDN_show_punycode" = true;
+          "security.family_safety.mode" = 0;
+        };
+      };
     };
   };
 
