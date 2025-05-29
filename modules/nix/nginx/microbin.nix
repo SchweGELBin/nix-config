388e8bfd8c2da3545f950cbd6ca7b8ba9b586fe7
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-29 23:13:03 +0200

    microbin: add admin pass

--- modules/nix/nginx/microbin.nix
+++ modules/nix/nginx/microbin.nix
@@ -1,16 +1,27 @@
-{ config, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 let
   cfg = config.sys.nginx;
   enable = cfg.enable && cfg.microbin.enable;
 
+  secrets = config.sops.secrets;
   vars = import ../vars.nix;
 in
 {
+  imports = [ inputs.sops-nix.nixosModules.default ];
+
   config = lib.mkIf enable {
     services = {
       microbin = {
         enable = true;
-        settings.MICROBIN_PORT = cfg.microbin.port;
+        settings = {
+          MICROBIN_ADMIN_PASSWORD = secrets.microbin.path;
+          MICROBIN_PORT = cfg.microbin.port;
+        };
       };
       nginx.virtualHosts."microbin.${vars.my.domain}" = {
         enableACME = true;
@@ -18,5 +29,6 @@ in
         locations."/".proxyPass = "http://localhost:${toString cfg.microbin.port}";
       };
     };
+    sops.secrets.microbin.owner = "microbin";
   };
 }
