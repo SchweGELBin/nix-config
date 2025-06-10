e205606e73dba571d57eb71dab349a9ef9795866
SchweGELBin <abramjannikmichael06@gmail.com>
2025-06-10 18:39:14 +0200

    add my cachix for nur

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -18,12 +18,14 @@ in
           "nix-command"
         ];
         substituters = [
+          "https://cachix.org"
           "https://hyprland.cachix.org"
           "https://nix-community.cachix.org"
         ];
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "schwegelbin.cachix.org-1:Ckh//WCg8vz3W1AzjD/QdYZ4VHA7ZU/q7nXb98IZ+TQ="
         ];
       };
     };
