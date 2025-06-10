e3c002c23d90c21853d2a19f23b006476102a5cf
SchweGELBin <abramjannikmichael06@gmail.com>
2025-06-10 18:40:36 +0200

    update cachix for nur

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -18,9 +18,9 @@ in
           "nix-command"
         ];
         substituters = [
-          "https://cachix.org"
           "https://hyprland.cachix.org"
           "https://nix-community.cachix.org"
+          "https://schwegelbin.cachix.org"
         ];
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
