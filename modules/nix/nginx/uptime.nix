214150ed378e3f9d88ce7a7d79494a8b2a10838d
SchweGELBin <abramjannikmichael06@gmail.com>
2025-07-03 18:25:32 +0200

    uptime: setup reverse proxy

--- modules/nix/nginx/uptime.nix
+++ modules/nix/nginx/uptime.nix
@@ -1,19 +1,20 @@
 { config, lib, ... }:
 let
   cfg = config.sys.nginx;
-  enable = cfg.enable && cfg.peertube.enable;
+  enable = cfg.enable && cfg.uptime.enable;
 in
 {
   config = lib.mkIf enable {
     services = {
+      nginx.virtualHosts.${cfg.uptime.fqdn} = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/".proxyPass = "http://localhost:${toString cfg.uptime.port}";
+      };
       uptime-kuma = {
         enable = true;
         appriseSupport = true;
         settings.PORT = toString cfg.uptime.port;
-      };
-      nginx.virtualHosts.${cfg.uptime.fqdn} = {
-        enableACME = true;
-        forceSSL = true;
       };
     };
   };
