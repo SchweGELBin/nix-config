61c4147ead036788f9f8a9a78186e30b18ec9ca9
SchweGELBin <abramjannikmichael06@gmail.com>
2025-07-16 17:09:35 +0200

    cache: disable hyprland if not enabled

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -19,10 +19,13 @@ in
           "flakes"
           "nix-command"
         ];
-        substituters = [
-          "https://hyprland.cachix.org"
-          "https://nix-community.cachix.org"
-        ];
+        substituters =
+          [
+            "https://nix-community.cachix.org"
+          ]
+          ++ lib.optionals (config.sys.pkgs.home.enable && config.sys.pkgs.home.wm == "hyprland") [
+            "https://hyprland.cachix.org"
+          ];
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
