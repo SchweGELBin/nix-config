5359d77ffc2423f3c7228ae69a1d72eee407ad93
SchweGELBin <abramjannikmichael06@gmail.com>
2025-11-25 00:40:19 +0100

    secrets: setup librechat

--- modules/nix/nginx/ollama.nix
+++ modules/nix/nginx/ollama.nix
@@ -7,7 +7,7 @@
 let
   cfg = config.sys.nginx;
   enable = cfg.enable && cfg.ollama.enable;
-  #secrets = config.sops.secrets;
+  secrets = config.sops.secrets;
 in
 {
   imports = [ inputs.sops-nix.nixosModules.default ];
@@ -28,6 +28,13 @@ in
     // lib.optionalAttrs cfg.ollama.web.enable {
       librechat = {
         enable = cfg.ollama.web.mode == "librechat";
+        credentials = {
+          CREDS_IV = secrets.librechat_creds-iv;
+          CREDS_KEY = secrets.librechat_creds-key;
+          JWT_REFRESH_SECRET = secrets.librechat_jwt-refresh-secret;
+          JWT_SECRET = secrets.librechat_jwt-secret;
+          MEILI_MASTER_KEY = secrets.librechat_meili-master-key;
+        };
         env.PORT = cfg.ollama.web.port;
       };
       nginx.virtualHosts.${cfg.ollama.web.fqdn} = {
@@ -44,6 +51,13 @@ in
         port = cfg.ollama.web.port;
       };
     };
-    #sops.secrets.ollama_env.owner = "ollama";
+    sops.secrets = {
+      librechat_creds-iv = "librechat";
+      librechat_creds-key = "librechat";
+      librechat_jwt-refresh-secret = "librechat";
+      librechat_jwt-secret = "librechat";
+      librechat_meili-master-key = "librechat";
+      ollama_env.owner = "ollama";
+    };
   };
 }
