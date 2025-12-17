ad059770e0482f92ddcc039765fa3f14fe9d86e5
SchweGELBin <abramjannikmichael06@gmail.com>
2025-12-17 20:47:36 +0100

    nginx(whoogle): create theme

--- modules/nix/nginx/whoogle.nix
+++ modules/nix/nginx/whoogle.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.sys.nginx;
   enable = cfg.enable && cfg.whoogle.enable;
@@ -13,6 +18,17 @@ in
       };
       whoogle-search = {
         enable = true;
+        extraEnv = {
+          WHOOGLE_CONFIG_STYLE = lib.mkIf config.sys.catppuccin.enable (
+            lib.readFile (
+              pkgs.fetchurl {
+                url = "https://raw.githubusercontent.com/catppuccin/whoogle/9d961dc6e2ac405fee18ee1da9a14db1f139db39/env/mocha.env";
+                hash = "sha256-P9cVZglIVdo4vxDJ3+Df9zcEO7pDqrFClzNUuAIcMkQ=";
+              }
+            )
+          );
+          WHOOGLE_CONFIG_THEME = "dark";
+        };
         port = cfg.whoogle.port;
       };
     };
