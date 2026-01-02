f2c4a21d8d38a7a33b94290d5508f6cc1c33277d
SchweGELBin <abramjannikmichael06@gmail.com>
2026-01-02 15:32:50 +0100

    greeter: use `start-hyprland`

--- modules/nix/greeter.nix
+++ modules/nix/greeter.nix
@@ -15,8 +15,7 @@ in
       settings = {
         default_session = {
           command = "${pkgs.greetd}/bin/agreety --cmd ${
-            # Change to `start-hyprland` in next version
-            if (config.sys.pkgs.home.wm == "hyprland") then "Hyprland" else config.sys.pkgs.home.wm
+            if (config.sys.pkgs.home.wm == "hyprland") then "start-hyprland" else config.sys.pkgs.home.wm
           }";
           user = vars.user.name;
         };
