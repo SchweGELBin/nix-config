319f9f0627d5ccb7db05210d2b6026334d8ba32b
SchweGELBin <abramjannikmichael06@gmail.com>
2026-01-07 13:47:00 +0100

    phone: cleanup hardware-configuration

--- hosts/phone/hardware-configuration.nix
+++ hosts/phone/hardware-configuration.nix
@@ -1,26 +1,5 @@
-{
-  config,
-  lib,
-  modulesPath,
-  ...
-}:
+{ lib, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-
-  boot = {
-    initrd.availableKernelModules = [
-      "ahci"
-      "nvme"
-      "sd_mod"
-      "usb_storage"
-      "usbhid"
-      "xhci_pci"
-    ];
-    kernelModules = [ "kvm-intel" ];
-  };
-
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
