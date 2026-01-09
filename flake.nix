8c4de5d146da4c234c5f57075c7f6117e0db9ba9
SchweGELBin <abramjannikmichael06@gmail.com>
2026-01-09 17:22:59 +0100

    flake: remove firefox-addons; update

--- flake.nix
+++ flake.nix
@@ -25,11 +25,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
