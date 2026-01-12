61405659238b4d214b38d3609402412112798f55
SchweGELBin <abramjannikmichael06@gmail.com>
2026-01-12 17:46:52 +0100

    theme: remove eval warning

--- modules/hm/theme.nix
+++ modules/hm/theme.nix
@@ -20,7 +20,9 @@ in
 
       cava.transparent = true;
       cursors.enable = cfg.catppuccin.cursors.enable;
-      firefox.profiles.${vars.user.name}.force = true;
+      firefox.profiles = lib.mkIf config.firefox.enable {
+        ${vars.user.name}.force = true;
+      };
       gtk.icon.enable = cfg.catppuccin.gtk.enable;
     };
 
