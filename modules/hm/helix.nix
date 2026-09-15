1209bfc10505dd1aedccc7c7b9dca557b33fc40d
SchweGELBin <schwegelbin@gmail.com>
2026-09-16 01:15:04 +0200

    helix: update config

--- modules/hm/helix.nix
+++ modules/hm/helix.nix
@@ -42,13 +42,27 @@ in
       ];
       settings = {
         editor = {
+          bufferline = "multiple";
           cursor-shape = {
-            normal = "block";
             insert = "bar";
+            normal = "block";
             select = "underline";
+          };
+          indent-guides = {
+            character = "▏";
+            render = true;
+            skip-levels = 1;
           };
           insert-final-newline = true;
           line-number = "relative";
+          soft-wrap.enable = true;
+          trim-final-newlines = true;
+          trim-trailing-whitespace = true;
+          whitespace.render = {
+            nbsp = "all";
+            nnbsp = "all";
+            tab = "all";
+          };
         };
         theme = lib.mkIf config.theme.catppuccin.enable "catppuccin-${vars.cat.flavor}";
       };
